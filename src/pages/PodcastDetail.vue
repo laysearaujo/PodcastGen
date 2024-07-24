8002a19c96d079635323b7e4acc597eaffad0d22
laysearaujo <lrsa@cin.ufpe.br>
2024-07-24 20:29:19 -0300

    feat: details podcast

--- src/pages/PodcastDetail.vue
+++ src/pages/PodcastDetail.vue
@@ -1,67 +1,79 @@
 <template>
-    <q-page class="q-pa-md">
-      <div class="q-gutter-md">
-        <div class="q-mb-md flex items-center justify-between q-mt-lg">
-          <div class="text-h4">
-            {{ podcast.title }}
-          </div>
-          <div class="flex gap-md">
-            <q-chip class="q-mb-sm" color="primary" text-color="white">
-              {{ podcast.category }}
-            </q-chip>
-            <q-chip class="q-mb-sm" color="secondary" text-color="white">
-              {{ podcast.releaseDate }}
-            </q-chip>
-          </div>
+  <q-page class="q-pa-md">
+    <div class="q-gutter-md">
+      <div class="q-mb-md flex items-center justify-between q-mt-lg">
+        <div class="text-h4">
+          {{ podcast.title }}
         </div>
-  
-        <!-- Container flexível para centralizar o áudio e o botão de download -->
-        <div class="audio-container q-mb-md">
-          <audio controls :src="podcast.audioUrl" class="audio-player">
-            Seu navegador não suporta o elemento <code>audio</code>.
-          </audio>
+        <div class="flex gap-md">
+          <q-chip class="q-mb-sm" color="primary" text-color="white">
+            {{ podcast.category }}
+          </q-chip>
+          <q-chip class="q-mb-sm" color="secondary" text-color="white">
+            {{ podcast.period }}
+          </q-chip>
+        </div>
+      </div>
+
+      <!-- Container flexível para centralizar o áudio e o botão de download -->
+      <div class="audio-container q-mb-md">
+        <audio controls :src="podcast.audioUrl" class="audio-player">
+          Seu navegador não suporta o elemento <code>audio</code>.
+        </audio>
+        <a :href="podcast.audioUrl" download>
           <q-btn
             icon="download"
-            :href="podcast.audioUrl"
-            download
             class="download-btn"
             flat
           />
-        </div>
-  
-        <div class="text-h6 q-mb-sm">Transcrição:</div>
-        <pre class="q-mb-md">{{ podcast.transcription }}</pre>
+        </a>
       </div>
-    </q-page>
+
+      <div class="text-h6 q-mb-sm">Transcrição:</div>
+      <div class="summary">{{ podcast.summary }}</div>
+    </div>
+  </q-page>
 </template>
-  
+
 <script setup>
-  import { ref, onMounted } from 'vue'
-  import { useRoute } from 'vue-router'
-  
-  const route = useRoute()
-  
-  const podcast = ref({
-  title: 'Podcast de Teste', 
-  category: 'Tecnologia',
-  releaseDate: '2024-07-15',
-  transcription: 'Esta é uma transcrição de exemplo para o podcast de teste.',
-  audioUrl: 'https://www.example.com/audio/test-podcast.mp3',
-  id: 23
+import { ref, onMounted, watch } from 'vue'
+import { useRoute } from 'vue-router'
+import axios from 'axios'
+
+const route = useRoute()
+const podcast = ref({})
+
+// Função para buscar os detalhes do podcast
+const fetchPodcastDetails = async (podcastId) => {
+  try {
+    const response = await axios.get(`https://podcast-gen-back.onrender.com/podcast/${podcastId}`)
+    podcast.value = response.data
+    console.log(podcast.value)
+  } catch (error) {
+    console.error('Erro ao buscar detalhes do podcast:', error)
+  }
+}
+
+// Observar mudanças no parâmetro de rota `id` e buscar os detalhes do podcast
+watch(() => route.params.id, (newId) => {
+  if (newId) {
+    fetchPodcastDetails(newId)
+  }
 })
-  
-  onMounted(() => {
-    // Simulação de recuperação dos dados do podcast
-    const podcastId = route.params.id
-    // Aqui você pode adicionar lógica para modificar o podcast com base no ID, se necessário
-    console.log(`Recuperando detalhes para o podcast com ID: ${podcastId}`)
-  })
+
+// Buscar detalhes do podcast ao montar o componente
+onMounted(() => {
+  const podcastId = route.params.id
+  if (podcastId) {
+    fetchPodcastDetails(podcastId)
+  }
+})
 </script>
 
 <style scoped>
 /* Estilo para o container do áudio e botão de download */
 .audio-container {
-  width: 60vw; /* Define a largura do container de áudio para 70% da largura da tela */
+  width: 60vw; /* Define a largura do container de áudio para 60% da largura da tela */
   display: flex;
   align-items: center;
   justify-content: center;
@@ -79,5 +91,11 @@
   box-shadow: none; /* Remove a sombra */
   cursor: pointer; /* Muda o cursor para indicar um botão */
   flex-shrink: 0; /* Impede o botão de encolher */
+}
+
+/* Estilo para a transcrição */
+.summary {
+  white-space: pre-wrap; /* Faz o texto quebrar automaticamente e preservar espaços em branco */
+  word-wrap: break-word; /* Faz o texto quebrar na borda da caixa, se necessário */
 }
 </style>
